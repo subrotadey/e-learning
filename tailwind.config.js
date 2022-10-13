defcf9916b07b93cad513ffb8d31ad00c110e940
Subrota Dey <subrotadey540@gmail.com>
2022-10-14 03:47:37 +0600

    file setup and navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        learningtheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
